e90e0d10bd38635f481bf24e52a8576df2dde279
Sergey Samohvalov <Samohvalov@tlpt.ru>
2019-02-18 20:06:30 +0300

    Edit code style

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@
  Посмотрите как работает forEach и повторите это поведение для массива, который будет передан в параметре array
  */
 function forEach(array, fn) {
-	for(var i = 0; i < array.length; i++) {
-		fn(array[i], i, array)
-	}
+    for (var i = 0; i < array.length; i++) {
+        fn(array[i], i, array)
+    }
 }
 
 /*
@@ -19,11 +19,13 @@ function forEach(array, fn) {
  Посмотрите как работает map и повторите это поведение для массива, который будет передан в параметре array
  */
 function map(array, fn) {
-	var newArr = []
-	for(var i = 0; i < array.length; i++) {
-		newArr[i] = fn(array[i], i ,array)
-	}
-	return newArr
+    var newArr = []
+
+    for (var i = 0; i < array.length; i++) {
+        newArr[i] = fn(array[i], i, array)
+    }
+
+    return newArr
 }
 
 /*
@@ -33,12 +35,14 @@ function map(array, fn) {
  Посмотрите как работает reduce и повторите это поведение для массива, который будет передан в параметре array
  */
 function reduce(array, fn, initial) {
-	let x = initial || array[0],
-		i = initial ? 0 : 1
-	for( ; i < array.length; i++) {
-		x = fn(x, array[i], i, array)
-	}
-	return x
+    let x = initial || array[0],
+        i = initial ? 0 : 1
+
+    for ( ; i < array.length; i++) {
+        x = fn(x, array[i], i, array)
+    }
+    
+    return x
 }
 
 /*
@@ -50,14 +54,17 @@ function reduce(array, fn, initial) {
    upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
  */
 function upperProps(obj) {
-	let newArr = []
-	for(let key in obj) {
-		key = key.toUpperCase()
-		newArr.push(key)
-	}
-	return newArr
-}
+    let newArr = []
 
+    for (let key in obj) {
+        if (key) {
+            key = key.toUpperCase()
+            newArr.push(key)
+        }
+    }
+
+    return newArr
+}
 
 /*
  Задание 5 *:
